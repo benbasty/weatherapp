554b5dc6f89fd49e00f39d4e7f650d938cce79c1
benbasty <benbasty@yahoo.fr>
2023-04-09 20:09:51 +0800

    find new api to fetch hourly forecast

--- src/components/services/weatherService.js
+++ src/components/services/weatherService.js
@@ -4,7 +4,7 @@ const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 const getWeatherData = (infoType, searchParams) => {
     const url = new URL(BASE_URL + "/" + infoType);
-    url.search = new URLSearchParams({...searchParams, appid:API_KEY});
+    url.search = new URLSearchParams({...searchParams, appid: API_KEY});
 
     return fetch(url)
         .then((res) => res.json())
@@ -20,7 +20,11 @@ const formatCurrentWeather = (data) => {
         weather,
         wind: { speed },
     } = data;
+
+
     const { main: details, icon } = weather[0];
+
+
     return {
         lat,
         lon,
@@ -41,7 +45,7 @@ const formatCurrentWeather = (data) => {
 };
 
 const formatForeCastWeather = (data) => {
-    let {timezone, daily, hourly} = data;
+    let { timezone, daily, hourly } = data;
     daily = daily.slice(1,6).map((d) => {
         return {
             title: formatToLocalTime(d.dt, timezone, "ccc"),
@@ -54,18 +58,25 @@ const formatForeCastWeather = (data) => {
             title: formatToLocalTime(d.dt, timezone, "hh:mm a"),
             temp: d.temp,
             icon: d.weather[0].icon
-        }
+        };
     });
     return { timezone, daily, hourly};
 }
 
 const getFormattedWeatherData = async (searchParams) => {
     const formattedCurrentWeather = await getWeatherData
-        ('weather', searchParams).then(formatCurrentWeather);
+        ('weather', 
+        searchParams
+        ).then(formatCurrentWeather);
     const {lat, lon} = formattedCurrentWeather;
+
     const formattedForeCastWeather = await getWeatherData('onecall',{
-        lat,lon, exclude: 'current,minutely,alerts', units: searchParams.units
+        lat,
+        lon, 
+        exclude: 'current,minutely,alerts', 
+        units: searchParams.units
     }).then(formatForeCastWeather)
+
     return {...formattedCurrentWeather,...formattedForeCastWeather}
 }
 
@@ -74,4 +85,5 @@ const formatToLocalTime = (
     zone,
     format = "cccc, dd LLL yyyy"
   ) => DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
+
 export default getFormattedWeatherData;
