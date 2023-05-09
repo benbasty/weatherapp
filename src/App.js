afc11d36d5ef59bb1d2809c7a23af378455ea794
benbasty <benbasty@yahoo.fr>
2023-05-09 17:18:43 +0800

    need fix links

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import './App.css';
 import Cities from '../src/components/cities/Cities';
 import Search from '../src/components/search/Search';
 import CurrentForecast from '../src/components/forecast/Current/CurrentForecast';
-import HourlyForecast from './components/forecast/Hourly/HourlyForecast';
+// import HourlyForecast from './components/forecast/Hourly/HourlyForecast';
 import DailyForecast from './components/forecast/Daily/DailyForecast';
 import { WEATHER_API_URL, WEATHER_API_KEY } from './components/services/api';
+import { GEO_API_URL, GEOApiOptions } from './components/services/api';
 import { useState } from 'react';
 // import getFormattedCurrentWeather from './components/services/weatherService';
 
@@ -29,20 +30,44 @@ function App() {
         setForecast({city:searchData.label ,...forecastResponse});
       })
       .catch((err) => console.log(err));
-    console.log(forecast);
+    // console.log(forecast);
+    // console.log(searchData);
 
   }
+
+  const setQuery = (setQuery) => {
+      return fetch(
+        `${GEO_API_URL}?minPopulation=1000000&namePrefix=${setQuery}`,
+        GEOApiOptions
+        )
+        .then((response) => response.json())
+        .then((response) => {
+            return {
+              options: response.data.map((city) => {
+                return {
+                  value: `${city.latitude} ${city.longitude}`,
+                  label: `${city.name}, ${city.countryCode}`
+                }
+              })
+            }
+          }
+        )
+        .catch((err) => console.error(err));
+  }
+  console.log(setQuery(setQuery));
   // const fetchWeather = async () => {
   //   const data = await getFormattedCurrentWeather({q:'guangzhou'});
   //   console.log(data);
   // };
   // fetchWeather();
 
+  
+
 
   //1:13
   return (
     <>
-      <Cities />
+      <Cities setQuery={setQuery}/>
       <Search onSearchChange={handleOnSearchChange}/>
       {currentWeather && <CurrentForecast data={currentWeather} />}
       {/* {forecast && <HourlyForecast data={forecast} />} */}
--- src/components/cities/Cities.js
+++ src/components/cities/Cities.js
@@ -1,15 +1,42 @@
 import React from 'react'
 import './Cities.css'
 
-const Cities = () => {
+const citiesList = [
+  {
+    id: 1,
+    title: 'London'
+  },
+  {
+    id: 2,
+    title: 'Sydney'
+  },
+  {
+    id: 3,
+    title: 'Beijing'
+  },
+  {
+    id: 4,
+    title: 'Toronto'
+  },
+  {
+    id: 5,
+    title: 'Paris'
+  }
+]
+
+const Cities = ({setQuery}) => {
   return (
     <div className='container cities'>
         <div className='city'>
-            <h4><a href='#city'>London</a></h4>
-            <h4><a href='#city'>Sydney</a></h4>
-            <h4><a href='#city'>Beijing</a></h4>
-            <h4><a href='#city'>Toronto</a></h4>
-            <h4><a href='#city'>Paris</a></h4>
+            {
+              citiesList.map((city) => {
+                return (
+                  <h4 key={city.id}>
+                    <a href='#city' onClick = {() => setQuery({q: city.title})} >{city.title}</a>
+                  </h4>
+                )
+              })
+            }
         </div>
     </div>
   )
